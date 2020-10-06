dc094626328823bc6aa70cf58b7155094069a6d2
Tabby Garcia <tabby.garcia1@gmail.com>
2020-10-06 15:19:11 -0500

    reverse int with parseint

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,20 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    const reversed = n
+    .toString()
+    .split('')
+    .reverse()
+    .join('');
+
+    if (n < 0) {
+        return parseInt(reversed) * -1;
+    }
+
+    return parseInt(reversed);
+
+}
 
 module.exports = reverseInt;
+ 
