1945c0213f2a009a25574ccaec419bc732d9453e
Tabby Garcia <tabby.garcia1@gmail.com>
2020-10-08 13:04:12 -0500

    FIZZBUZZ yayyyy super easy and clean

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,31 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+
+//console log
+//for loop
+//multiple
+//specific operator(modulo) for remainders eg:11 % 3 === 0 
+
+
+
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+   //   is the number a multiple of 3 or 5?
+   if (i % 3 === 0 && i % 5 === 0) {
+       console.log('fizzbuzz');
+       //is the number a multiple of 3?
+   } else if (i % 3 === 0) {
+    console.log('fizz');
+    //is the number a multiple of 5?
+   } else if (i % 5 === 0){
+       console.log('buzz');
+   } else {
+       //if none then print out the number or i
+      console.log(i); 
+   }
+
+  }  
+}
 
 module.exports = fizzBuzz;
