0e152fad263b3ddea620944c7a54e6998322ac59
Tabby Garcia <tabby.garcia1@gmail.com>
2020-10-11 19:51:08 -0500

    palindrome practice

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -10,14 +10,39 @@
 
 
 
+
+
+
+
+
+
+
+
 function palindrome(str) {
     const reversed = str.split('').reverse().join('');
-
-    return str === reversed;
-
+   return reversed === str;
 }
 
 module.exports = palindrome;
+
+
+
+
+
+
+
+
+
+
+
+// function palindrome(str) {
+//     const reversed = str.split('').reverse().join('');
+
+//     return str === reversed;
+
+// }
+
+// module.exports = palindrome;
 
 
 
